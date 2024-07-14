bc607110f7903a441197e32fcf17dc3fb17f9a28
kozdailyy <kazockparfait02@gmail.com>
2024-07-14 21:43:40 +0200

    Site Interupt at whatWebsite's section

--- src/containers/whatWebsite/WhatWebsite.jsx
+++ src/containers/whatWebsite/WhatWebsite.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import "./whatWebsite.css";
+import { Feature } from "../../components";
 
 const WhatWebsite = () => {
-  return <div>WhatWebsite</div>;
+  return (
+    <div className="modernReact__whatWebsite section__margin" id="whatWebsite">
+      <div className="modernReact__whatWebsite-feature">
+        <Feature
+          title="What is GPT-3"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+        />
+      </div>
+      <div className="modernReact__whatWebsite-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="modernReact__whatWebsite-container">
+        <Feature
+          title="Chatbots"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought."
+        />
+        <Feature
+          title="Knowledgebase"
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+        />
+        <Feature
+          title="Education"
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default WhatWebsite;
